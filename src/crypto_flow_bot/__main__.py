8406ed74e4e668443e0980fd8b26bf02f300ed33
hubievs13-web <hubievs13@gmail.com>
2026-06-23 19:30:29 +0300

    Enable signal visibility during startup

--- src/crypto_flow_bot/__main__.py
+++ src/crypto_flow_bot/__main__.py
@@ -1,3 +1,7 @@
+import crypto_flow_bot.signal_visibility as _signal_visibility
+
+_signal_visibility.install_trade_signal_visibility_patch()
+
 from crypto_flow_bot.main import main
 
 if __name__ == "__main__":
